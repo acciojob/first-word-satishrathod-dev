ce50334330ddb785b6a324059aa4215f77a90187
satishrathod-dev <167071223+satishrathod-dev@users.noreply.github.com>
2024-05-20 16:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	if(str.trim() === ""){
+		return str;
+	}
+	
+	const firstSpaceIndex = str.indexOf(" ");
+
+	if(firstSpaceIndex === -1){
+		return str;
+	}
+	else{
+		return str.splice(0, firstSpaceIndex);
+	}
 }
 
 // Do not change the code below
